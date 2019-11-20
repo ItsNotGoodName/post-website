32317db031cbd6e6c8a8fbc4c8d488c6f23b82b6
ItsNotGoodName <gurnaindeol@mgail.com>
2019-11-19 19:59:27 -0800

    Add start output

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync('cert_key/key.pem', 'utf8');
 var certificate = fs.readFileSync('cert_key/cert.pem', 'utf8');
+var port = 8443;
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
@@ -18,4 +19,6 @@ app.get('/', (req, res)=> {
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(8443);
+httpsServer.listen(port, ()=>{
+	console.log("Server running on port: " + port);
+});
