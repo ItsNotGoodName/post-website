d6f8242e3656ffad46bc53b1a4d8decb4743e81c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 17:54:00 -0800

    added http server and start script

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync('cert_key/key.pem', 'utf8');
 var certificate = fs.readFileSync('cert_key/cert.pem', 'utf8');
-var port = 8443;
+var httpPort = 8080
+var httpsPort = 8443;
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
@@ -19,6 +20,10 @@ app.get('/', (req, res)=> {
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(port, ()=>{
-	console.log("Server running on port: " + port);
+httpServer.listen(httpPort, ()=>{
+	console.log("HTTP server running on port: " + httpPort);
+});
+
+httpsServer.listen(httpsPort, ()=>{
+	console.log("HTTPS server running on port: " + httpsPort);
 });
