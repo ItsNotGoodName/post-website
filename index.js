36eb3e9968e36261dc1fb0ad2d2bee10424d8e1c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-22 16:02:51 -0800

    add check for cert_key

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
+const path = require('path')
 const http = require('http');
 const https = require('https');
-const privateKey  = fs.readFileSync('cert_key/key.pem', 'utf8');
-const certificate = fs.readFileSync('cert_key/cert.pem', 'utf8');
 require('dotenv').config()
 
 const httpPort = process.env.HTTPPORT || 8080
 const httpsPort = process.env.HTTPSPORT || 8443;
-
-const credentials = {key: privateKey, cert: certificate};
+const cert_key_path = "cert_key";
 
 const app = require('./src');
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(httpPort, ()=>{
 	console.log("HTTP server running on port: " + httpPort);
 });
 
-httpsServer.listen(httpsPort, ()=>{
-	console.log("HTTPS server running on port: " + httpsPort);
-});
+if (fs.existsSync(cert_key_path)) {
+	const privateKey  = fs.readFileSync('cert_key/key.pem', 'utf8');
+	const certificate = fs.readFileSync('cert_key/cert.pem', 'utf8');
+
+	const credentials = {key: privateKey, cert: certificate};
+	const httpsServer = https.createServer(credentials, app);
+
+	httpsServer.listen(httpsPort, ()=>{
+		console.log("HTTPS server running on port: " + httpsPort);
+	});
+}
