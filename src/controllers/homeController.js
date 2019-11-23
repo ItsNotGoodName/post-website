c9e987a90be049707d651b2b448ae25712c6e03c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 15:25:49 -0800

    Clean up

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const services = require('../services')
-const counterService = services.counterService
 const postService = services.postService
-
 
 router.get('/', (req, res)=> {
     res.render('index', {posts: postService.getPosts()});
@@ -14,12 +12,13 @@ router.get('/post', (req, res)=>{
 
 router.post('/post', (req, res)=>{
     console.log(req.body)
-    if (req.body.title == undefined || req.body.title == '' ||
-        req.body.body == undefined || req.body.body == '') {
+    title = req.body.title;
+    body = req.body.body;
+    if (title == undefined || title == '' || body == undefined || body == '') {
         res.redirect('/post');
         return;
     }
-    postService.addPost(req.body.title, req.body.body)
+    postService.addPost(title, body)
     res.redirect('/');
 })
 
