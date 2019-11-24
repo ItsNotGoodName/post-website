953f759720389374702084b010bc872e7be1da0b
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 21:34:56 -0800

    add production logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const fs = require('fs')
 const path = require('path');
 const app = express();
-const logger = require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 
 // Views
@@ -11,13 +12,12 @@ app.set('view engine', 'ejs');
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.urlencoded({
     extended: true
 }));
 
-// Redirect to HTTPS if in production
 if(process.env.ENVIROMENT=='production'){
+    // Redirect to HTTPS if in production
     function requireHTTPS(req, res, next) {
         if (!req.secure) {
             return res.redirect('https://' + req.get('host') + req.url);
@@ -25,6 +25,11 @@ if(process.env.ENVIROMENT=='production'){
         next();
     }
     app.use(requireHTTPS);
+
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log'), { flags: 'a' });
+    app.use(morgan('combined', {stream: accessLogStream}))
+}else{
+    app.use(morgan('dev'));
 }
 
 // Controllers
