ba1047df9d3c2a38b4539776038001d68fd790a6
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 16:08:23 -0800

    Add gulp and deps

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon')
+require('dotenv').config()
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "127.0.0.1:" + process.env.HTTPPORT || 8080
+    });
+});
+
+gulp.task('start', function (done) {
+  nodemon({
+    script: 'app.js'
+  , ext: 'js'
+  , env: { 'NODE_ENV': 'development' }
+  , done: done
+  })
+})
+
+gulp.task('default', gulp.parallel('browser-sync', 'start'));
