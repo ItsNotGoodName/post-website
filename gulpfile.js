0187bc4c09f12fb824c0b26775dc81da2bb24aa5
Gurnain Deol <gurnaindeol@gmail.com>
2019-11-26 10:58:26 -0800

    Watch ejs for browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,19 @@ const browserSync = require('browser-sync').create();
 const nodemon = require('gulp-nodemon')
 require('dotenv').config()
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "127.0.0.1:" + process.env.HTTPPORT || 8080
-    });
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    proxy: "127.0.0.1:" + process.env.HTTPPORT || 8080,
+  });
+  gulp.watch("src/views/**/*.ejs").on('change', browserSync.reload);
+
 });
 
 gulp.task('start', function (done) {
   nodemon({
-    script: 'app.js'
-  , ext: 'js ejs env'
-  , done: done
+    script: 'app.js',
+    ext: 'js ejs env',
+    done: done
   })
 })
 
