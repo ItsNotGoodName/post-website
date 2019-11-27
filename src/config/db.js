8461244fbfbf4985ca78ee1d8243a482fcf061cf
BuildTools <unconfigured@null.spigotmc.org>
2019-11-26 21:34:36 -0800

    Changed mongoose url

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://' + process.env.MONGO_URL + '/' + process.env.MONGO_DB, {
+mongoose.connect('mongodb+srv://'+ process.env.MONGO_USER + ':' + process.env.MONGO_PASS + "@" + process.env.MONGO_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 }).then(
