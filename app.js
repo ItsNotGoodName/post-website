2266ea7b2a67d50ff942db77e86278fb5fd1182d
Gurnain Deol <gurnaindeol@gmail.com>
2019-11-26 18:03:04 -0800

    add port env for heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const db = require('./src/config/db').connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
-	const httpPort = process.env.HTTPPORT || 8080
+	const httpPort = process.env.PORT || process.env.HTTPPORT || 8080
 	const httpsPort = process.env.HTTPSPORT || 8443;
 	const cert_key_path = "cert_key";
 
