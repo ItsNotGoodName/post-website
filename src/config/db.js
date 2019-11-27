2da67ec1866ffd2dd6903e017ada37bbbc364b3f
Gurnain Deol <gurnaindeol@gmail.com>
2019-11-26 21:40:03 -0800

    Fixed db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://'+ process.env.MONGO_USER + ':' + process.env.MONGO_PASS + "@" + process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_PREPEND +'://'+ process.env.MONGO_USER + ':' + process.env.MONGO_PASS + "@" + process.env.MONGO_URL + '/' + process.env.MONGO_DB, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 }).then(
