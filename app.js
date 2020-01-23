ede157d9a870a2d20809d85a94ad498bf5eadd6f
itsnotgoodname <gurnaindeol@gmail.com>
2020-01-23 15:31:25 -0800

    Removed useless stuff

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
 const http = require('http');
-const https = require('https');
 require('dotenv').config()
 const db = require('./src/config/db').connection;
 
@@ -8,8 +6,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
 	const httpPort = process.env.PORT || 8080
-	const httpsPort = process.env.HTTPSPORT || 8443;
-	const cert_key_path = "cert_key";
 
 	const app = require('./src');
 
@@ -18,19 +14,4 @@ db.once('open', function () {
 	httpServer.listen(httpPort, () => {
 		console.log("HTTP server running on port: " + httpPort);
 	});
-
-	if (fs.existsSync(cert_key_path)) {
-		const privateKey = fs.readFileSync('cert_key/key.pem', 'utf8');
-		const certificate = fs.readFileSync('cert_key/cert.pem', 'utf8');
-
-		const credentials = {
-			key: privateKey,
-			cert: certificate
-		};
-		const httpsServer = https.createServer(credentials, app);
-
-		httpsServer.listen(httpsPort, () => {
-			console.log("HTTPS server running on port: " + httpsPort);
-		});
-	}
 });
